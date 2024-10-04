26be29bd386141f8fd0a08849773c64e8072626a
derbe <derbely887@gmail.com>
2024-10-04 11:08:59 +0100

    microservices setup

---
+++ inventory-service/src/main/java/com/example/inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,33 @@
+package com.example.inventory_service;
+
+import com.example.inventory_service.model.Inventory;
+import com.example.inventory_service.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("k");
+			inventory.setQuantity(123);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("android");
+			inventory2.setQuantity(456);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory2);
+
+		};
+	}
+}
